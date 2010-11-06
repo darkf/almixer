8b52246d0944f23e922f42b4cdb58cb8e7b3eb5e
Eric Wing <ewing . public |-at-| gmail . com>
2010-11-06 00:44:35 -0700

    Added copyright.

--- LinkedList.h
+++ LinkedList.h
@@ -1,3 +1,27 @@
+/*
+ LinkedList
+ Copyright (C) 2010  Eric Wing <ewing . public @ playcontrol.net>
+ 
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Library General Public
+ License as published by the Free Software Foundation; either
+ version 2 of the License, or (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Library General Public License for more details.
+ 
+ You should have received a copy of the GNU Library General Public
+ License along with this library; if not, write to the Free
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+/* I'm so sick of constantly rewriting linked lists in C. 
+So here's some initial work to write a very simple reusable implementation.
+The API could use a lot more functions, but I'll add them as I need them.
+*/
+
 #ifndef C_LINKED_LIST_H
 #define C_LINKED_LIST_H
 
