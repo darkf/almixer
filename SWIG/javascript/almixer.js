1229d8b55309e13f715820c4c7421fbb68f3cdbc
Eric Wing <ewing . public |-at-| gmail . com>
2013-11-07 17:16:45 -0800

    Enhanced Javascript bootstrap script to allow optional parameters (omitting channel and source for Play*) and omitting num_loops for PlayChannel and PlaySource. Changed callback event names to channel, alsource, finishedNaturally. Allow userData to be passed in as final parameter of Play* for onComplete callbacks. Bug fixes for testing empty options_table; I didn't realize null and undefined were separate things in JavaScript.

--- SWIG/javascript/almixer.js
+++ SWIG/javascript/almixer.js
@@ -5,8 +5,10 @@ var s_JSALmixerTiProxy;
 
 var playbackFinishedCallbackContainer;
 
-function JSALmixer_Initialize() {
-	if(s_JSALmixerIsInitialized) {
+function JSALmixer_Initialize()
+{
+	if(s_JSALmixerIsInitialized)
+	{
 		return;
 	}
 	s_JSALmixerIsInitialized = true;
@@ -21,7 +23,8 @@ function JSALmixer_Initialize() {
 
 	s_JSALmixerDataChannelTable = {};
 
-	almixer_ti_proxy.addEventListener('ALmixerSoundPlaybackFinished', function(e) {
+	almixer_ti_proxy.addEventListener('ALmixerSoundPlaybackFinished', function(e)
+	{
 		var which_channel = e.which_channel;
 		var saved_table = s_JSALmixerDataChannelTable[which_channel];
 
@@ -31,10 +34,11 @@ function JSALmixer_Initialize() {
 		{
 			name:"ALmixer",
 			type:"completed",
-			which_channel:which_channel,
-			channel_source:e.channel_source,
-			completed:e.completed,
+			channel:which_channel,
+			alsource:e.channel_source,
+			finishedNaturally:e.finished_naturally,
 			handle:saved_table.soundHandle,
+			userData:saved_table.userData
 		};
 
 		// We can now free our saved reference
@@ -50,10 +54,25 @@ function JSALmixer_Initialize() {
 	});
 
 	ALmixer._original.PlayChannelTimed = ALmixer.PlayChannelTimed;
-	ALmixer.PlayChannelTimed = function(which_channel, sound_handle, num_loops, duration, on_complete) {
+	ALmixer.PlayChannelTimed = function(which_channel, sound_handle, num_loops, duration, on_complete, user_data)
+	{
+		// Allow which_channel to be omitted (automatically assumes -1).
+		// This means all parameters need to shift over
+		if(typeof(which_channel) === "object")
+		{
+			// Careful: order of the shift matters or we clobber a variable we need to save
+			user_data = on_complete;
+			on_complete = duration;
+			duration = num_loops;
+			num_loops = sound_handle;
+			sound_handle = which_channel;
+			which_channel = -1;
+		}
+
 		var playing_channel = ALmixer._original.PlayChannelTimed(which_channel, sound_handle, num_loops, duration);
 		// Do only if playing succeeded
-		if(playing_channel > -1) {
+		if(playing_channel > -1)
+		{
 			// Save the sound_handle to solve two binding problems.
 			// Problem 1: Garbage collection. The object looks like it disappears and resurrects later.
 			// We can't let the garbage collector collect the object while it is playing.
@@ -68,16 +87,42 @@ function JSALmixer_Initialize() {
 			// In the callback, we can retrieve the data by looking up the channel which solves the pointer problem.
 			// Problem 3: We would also like to make the API nicer for Javascript and let people pass in anonymous callback functions for each channel.
 			// This table structure will let us also keep around their callback.
-			s_JSALmixerDataChannelTable[playing_channel] = { soundHandle:sound_handle, onComplete:on_complete };
+			s_JSALmixerDataChannelTable[playing_channel] = { soundHandle:sound_handle, onComplete:on_complete, userData:user_data };
 		}
 		return playing_channel;
 	};
 
 	ALmixer._original.PlayChannel = ALmixer.PlayChannel;
-	ALmixer.PlayChannel = function(which_channel, sound_handle, num_loops, on_complete) {
+	ALmixer.PlayChannel = function(which_channel, sound_handle, num_loops, on_complete, user_data)
+	{
+		// Allow which_channel to be omitted (automatically assumes -1).
+		// This means all parameters need to shift over
+		// Assumption is that the following parameter MUST be the ALmixer_Data which is an object.
+		if(typeof(which_channel) === "object")
+		{
+			// Careful: order of the shift matters or we clobber a variable we need to save
+			user_data = on_complete;
+			on_complete = num_loops;
+			num_loops = sound_handle;
+			sound_handle = which_channel;
+			which_channel = -1;
+		}
+
+		// Also allow num_loops to be omitted (automatically assumes 0).
+		// This means all parameters following need to shift over
+		// Note: Because the on_complete is optional, we explicitly test for the number and blindly shift.
+		if(typeof(num_loops) !== "number")
+		{
+			// Careful: order of the shift matters or we clobber a variable we need to save
+			user_data = on_complete;
+			on_complete = num_loops;
+			num_loops = 0;
+		}
+
 		var playing_channel = ALmixer._original.PlayChannel(which_channel, sound_handle, num_loops);
 		// Do only if playing succeeded
-		if(playing_channel > -1) {
+		if(playing_channel > -1)
+		{
 			// Save the sound_handle to solve two binding problems.
 			// Problem 1: Garbage collection. The object looks like it disappears and resurrects later.
 			// We can't let the garbage collector collect the object while it is playing.
@@ -92,16 +137,31 @@ function JSALmixer_Initialize() {
 			// In the callback, we can retrieve the data by looking up the channel which solves the pointer problem.
 			// Problem 3: We would also like to make the API nicer for Javascript and let people pass in anonymous callback functions for each channel.
 			// This table structure will let us also keep around their callback.
-			s_JSALmixerDataChannelTable[playing_channel] = { soundHandle:sound_handle, onComplete:on_complete };
+			s_JSALmixerDataChannelTable[playing_channel] = { soundHandle:sound_handle, onComplete:on_complete, userData:user_data };
 		}
 		return playing_channel;
 	};
 
 	ALmixer._original.PlaySourceTimed = ALmixer.PlaySourceTimed;
-	ALmixer.PlaySourceTimed = function(al_source, sound_handle, num_loops, duration, on_complete) {
+	ALmixer.PlaySourceTimed = function(al_source, sound_handle, num_loops, duration, on_complete, user_data)
+	{
+		// Allow which_channel to be omitted (automatically assumes 0).
+		// This means all parameters need to shift over
+		if(typeof(al_source) === "object")
+		{
+			// Careful: order of the shift matters or we clobber a variable we need to save
+			user_data = on_complete;
+			on_complete = duration;
+			duration = num_loops;
+			num_loops = sound_handle;
+			sound_handle = al_source;
+			al_source = 0;
+		}
+
 		var playing_source = ALmixer._original.PlaySourceTimed(al_source, sound_handle, num_loops, duration);
 		// Do only if playing succeeded
-		if(playing_source > 0) {
+		if(playing_source > 0)
+		{
 			// convert source to channel
 			var playing_channel = ALmixer.GetChannel(playing_source);
 			
@@ -119,16 +179,41 @@ function JSALmixer_Initialize() {
 			// In the callback, we can retrieve the data by looking up the channel which solves the pointer problem.
 			// Problem 3: We would also like to make the API nicer for Javascript and let people pass in anonymous callback functions for each channel.
 			// This table structure will let us also keep around their callback.
-			s_JSALmixerDataChannelTable[playing_channel] = { soundHandle:sound_handle, onComplete:on_complete };
+			s_JSALmixerDataChannelTable[playing_channel] = { soundHandle:sound_handle, onComplete:on_complete, userData:user_data };
 		}
 		return playing_source;
 	};
 
 	ALmixer._original.PlaySource = ALmixer.PlaySource;
-	ALmixer.PlaySourceTimed = function(al_source, sound_handle, num_loops, on_complete) {
+	ALmixer.PlaySource = function(al_source, sound_handle, num_loops, on_complete, user_data)
+	{
+		// Allow which_channel to be omitted (automatically assumes 0).
+		// This means all parameters need to shift over
+		if(typeof(al_source) === "object")
+		{
+			// Careful: order of the shift matters or we clobber a variable we need to save
+			user_data = on_complete;
+			on_complete = num_loops;
+			num_loops = sound_handle;
+			sound_handle = al_source;
+			al_source = 0;
+		}
+
+		// Also allow num_loops to be omitted (automatically assumes 0).
+		// This means all parameters following need to shift over
+		// Note: Because the on_complete is optional, we explicitly test for the number and blindly shift.
+		if(typeof(num_loops) !== "number")
+		{
+			// Careful: order of the shift matters or we clobber a variable we need to save
+			user_data = on_complete;
+			on_complete = num_loops;
+			num_loops = 0;
+		}
+
 		var playing_source = ALmixer._original.PlaySource(al_source, sound_handle, num_loops);
 		// Do only if playing succeeded
-		if(playing_source > 0) {
+		if(playing_source > 0)
+		{
 			// convert source to channel
 			var playing_channel = ALmixer.GetChannel(playing_source);
 			
@@ -146,40 +231,56 @@ function JSALmixer_Initialize() {
 			// In the callback, we can retrieve the data by looking up the channel which solves the pointer problem.
 			// Problem 3: We would also like to make the API nicer for Javascript and let people pass in anonymous callback functions for each channel.
 			// This table structure will let us also keep around their callback.
-			s_JSALmixerDataChannelTable[playing_channel] = { soundHandle:sound_handle, onComplete:on_complete };
+			s_JSALmixerDataChannelTable[playing_channel] = { soundHandle:sound_handle, onComplete:on_complete, userData:user_data };
 		}
 		return playing_source;
 	};
 
 
-	function JSALmixerPlaySound(sound_handle, options_table) {
+	function JSALmixerPlaySound(sound_handle, options_table)
+	{
 		var which_channel = -1;
+		var al_source = 0;
 		var num_loops = 0;
 		var duration = -1;
 		var on_complete = null;
-		if(null !== options_table) {
-			if(null !== options_table.which_channel) {
-				which_channel = options_table.which_channel;
-			} else if(null !== options_table.channel_source) {
-				which_channel = ALmixer.GetChannel(options_table.channel_source);
+		var user_data = null;
+		var using_source_instead_of_channel = false;
+		if(options_table)
+		{
+			if(options_table.channel)
+			{
+				which_channel = options_table.channel;
+			}
+			else if(options_table.alsource)
+			{
+				which_channel = ALmixer.GetChannel(options_table.alsource);
+				using_source_instead_of_channel = true;
 			}
 
 
-			if(null !== options_table.loops) {
+			if(options_table.loops)
+			{
 				num_loops = options_table.loops;
 			} 
-			if(null !== options_table.duration) {
+			if(options_table.duration)
+			{
 				duration = options_table.duration;
 			}
-			if(null !== options_table.onComplete) {
+			if(options_table.onComplete)
+			{
 				on_complete = options_table.onComplete;
 			}
-			
+			if(options_table.userData)
+			{
+				user_data = options_table.userData;
+			}
 		}
 		// Could call overridden version and omit code instead.
 		var playing_channel = ALmixer._original.PlayChannelTimed(which_channel, sound_handle, num_loops, duration);
 		// Do only if playing succeeded
-		if(playing_channel > -1) {
+		if(playing_channel > -1)
+		{
 			// Save the sound_handle to solve two binding problems.
 			// Problem 1: Garbage collection. The object looks like it disappears and resurrects later.
 			// We can't let the garbage collector collect the object while it is playing.
@@ -194,13 +295,23 @@ function JSALmixer_Initialize() {
 			// In the callback, we can retrieve the data by looking up the channel which solves the pointer problem.
 			// Problem 3: We would also like to make the API nicer for Javascript and let people pass in anonymous callback functions for each channel.
 			// This table structure will let us also keep around their callback.
-			s_JSALmixerDataChannelTable[playing_channel] = { soundHandle:sound_handle, onComplete:on_complete };
-			
+			s_JSALmixerDataChannelTable[playing_channel] = { soundHandle:sound_handle, onComplete:on_complete, userData:user_data };
 		}
 
 		// I wish Javascript had multiple return values
-		return playing_channel;
-
+		if(using_source_instead_of_channel)
+		{
+			var playing_source = 0;
+			if(playing_channel > -1)
+			{
+				playing_source = ALmixer.GetSource(playing_channel);
+			}
+			return playing_source;
+		}
+		else
+		{
+			return playing_channel;
+		}
 	}
 
 	ALmixer.util.Play = JSALmixerPlaySound;
